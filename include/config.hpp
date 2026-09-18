60e1a767e303b3cc68308545cb7415fab1081ae2
takeahike603 <ajc3023@g.rit.edu>
2026-09-18 17:49:11 -0400

    documentation updates

--- include/config.hpp
+++ include/config.hpp
@@ -4,20 +4,6 @@
 #include <string.h>
 #include <GxEPD2_BW.h>
 
-inline const char* ssid = "REDDYLLC-CHESTER";
-inline const char* password = "Che$ter@29";
-inline String calScriptUrl = "https://script.google.com/macros/s/AKfycbzj14kizI2lhFPq-zensrILQ7Yl5KtXEMLRQ7hx_Z9xBKb1lRXeMzNhy-u_zq7ajdcs/exec";
-inline String taskScriptUrl = "https://script.google.com/macros/s/AKfycbzZznhWzbJWRDTH1w-YUr5-dbsYX-2nRXSFbxoc-nmNTirIczZRm85dHkb1alaoq6M_ng/exec";
-inline const char* tz_string = "EST5EDT,M3.2.0,M11.1.0";
-inline bool calendarDataLoaded = false;
-inline bool taskDataLoaded = false;
-inline const char* ntpServer = "pool.ntp.org";
-inline const long  gmtOffset_sec = -18000;    // EST is GMT -5 hours (-5 * 3600 = -18000)
-inline const int   daylightOffset_sec = 3600; // 1 hour daylight saving offset (in seconds)
-inline int taskYTracker = 85;
-inline int todayHeaderPrinted = 0;
-inline int tomorrowHeaderPrinted = 0;
-inline int laterHeaderPrinted = 0;
 
 // ----------- Pins -----------
 #define EPD_DC     6
@@ -27,6 +13,30 @@ inline int laterHeaderPrinted = 0;
 #define EPD_RST     20
 #define EPD_BUSY    19
 #define ONBOARD_LED 15
+
+// Wifi name and password
+inline const char* ssid = "REDDYLLC-CHESTER";
+inline const char* password = "Che$ter@29";
+
+// URL to google calendar and tasks script (See README for instructions)
+inline String calScriptUrl = "https://script.google.com/macros/s/AKfycbzj14kizI2lhFPq-zensrILQ7Yl5KtXEMLRQ7hx_Z9xBKb1lRXeMzNhy-u_zq7ajdcs/exec";
+inline String taskScriptUrl = "https://script.google.com/macros/s/AKfycbzZznhWzbJWRDTH1w-YUr5-dbsYX-2nRXSFbxoc-nmNTirIczZRm85dHkb1alaoq6M_ng/exec";
+
+// Time zone string (Find yours here: https://support.cyberdata.net/portal/en/kb/articles/010d63c0cfce3676151e1f2d5442e311)
+inline const char* tz_string = "EST5EDT,M3.2.0,M11.1.0";
+
+// Network time protocol server, for getting current date and time
+inline const char* ntpServer = "pool.ntp.org";
+inline const long  gmtOffset_sec = -18000;    // EST is GMT -5 hours (-5 * 3600 = -18000)
+inline const int   daylightOffset_sec = 3600; // 1 hour daylight saving offset (in seconds)
+
+// Status variables for tracking certain locations and data
+inline bool calendarDataLoaded = false;
+inline bool taskDataLoaded = false;
+inline int taskYTracker = 85;
+inline int todayHeaderPrinted = 0;
+inline int tomorrowHeaderPrinted = 0;
+inline int laterHeaderPrinted = 0;
 
 inline GxEPD2_BW<GxEPD2_583_T8, GxEPD2_583_T8::HEIGHT> display(GxEPD2_583_T8(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 
--- src/timing.cpp
+++ src/timing.cpp
@@ -23,6 +23,13 @@ time_t convert_utc_tm_to_time_t(struct tm *tm) {
   return utc_time;
 }
 
+/***************************************************************
+* @brief gets today's date from local network
+* 
+* @param date: array of characters to store date string in
+*        dateSize: size of character array
+* @return void
+***************************************************************/
 void getTodaysDate(char* date, size_t dateSize) {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
@@ -34,6 +41,13 @@ void getTodaysDate(char* date, size_t dateSize) {
   }
 }
 
+/************************************************************
+* @brief get tomorrow's date from local network
+* 
+* @param date: array of characters to store date string in
+*        dateSize: size of character array
+* @return void
+**************************************************************/
 void getTomorrowsDate(char* date, size_t dateSize) {
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
@@ -48,6 +62,14 @@ void getTomorrowsDate(char* date, size_t dateSize) {
   }
 }
 
+/************************************************************
+* @brief returns the amount of time in minutes between two 
+*        time structures
+* 
+* @param startTime: reference time
+*        endTime: end time
+* @return long: time in minutes between two entered times
+**************************************************************/
 long timePeriod (struct tm *startTime, struct tm *endTime){
   time_t startSeconds = mktime(startTime);
   time_t endSeconds = mktime(endTime);
@@ -56,6 +78,13 @@ long timePeriod (struct tm *startTime, struct tm *endTime){
 }
 
 // Returns days between date1 and date2 (positive if date1 is before date2)
+/************************************************************
+* @brief Compares the number of days between two dates
+* 
+* @param date1: reference date
+*        date2: end date
+* @return int: number of days between date1 and 2
+**************************************************************/
 int compareDates(struct tm date1, struct tm date2){
   struct tm date1Strip = {0}; 
   struct tm date2Strip = {0};
